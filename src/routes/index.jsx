8c6294bdf253efca0d013106512347bce5bd10f2
puna1175 <myselfpunam.bhuiyan@gmail.com>
2023-11-14 21:47:08 +0600

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      Hello From Punam Bhuiyan 
+        He is trying to do react router using VITE 
+      <br />
+      Check out{" "}
+      <a href="https://www.facebook.com/profile.php?id=100075836989269">
+        Punam Bhuiyan
+      </a>
+      .
+    </p>
+  );
+}
